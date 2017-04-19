c7d5460389d9248f6160c290fce6322f6bff60ad
JJohn86 <johnstonjamese@gmail.com>
2017-04-19 09:11:58 +0900

    Update dungeon_game.py

--- dungeon_game.py
+++ dungeon_game.py
@@ -3,19 +3,6 @@ import os
 
 
 
-# pick random location for exit
-# pick random location for monster
-
-# check for win/loss
-# clear screen and re-draw grid
-
-
-
-
-
-
-
-
 CELLS = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
          (0, 1), (1, 1), (2, 1), (3, 1), (4, 1),
          (0, 2), (1, 2), (2, 2), (3, 2), (4, 2),
